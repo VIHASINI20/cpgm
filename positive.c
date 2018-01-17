e850498fa1a7554fa6c1f769c9753990cf0d4bf9
VIHASINI20 <35221143+VIHASINI20@users.noreply.github.com>
2018-01-17 16:46:58 +0530

    c program
    positive or negative

---
+++ positive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include(conio.h>
+int main()
+{
+double number;
+printf("Enter a number:");
+scanf("% if ",&number);
+if(number <=0.0)
+{
+if(number ==0.0)
+print(" you enetered 0");
+else
+print(" you entered a negative number:");
+}
+else
+print(" you entered a positive number:");
+return0;
+}

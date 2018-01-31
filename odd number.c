07ec80c25d9a75691251deedc87298cc4a0e0145
VIHASINI20 <35221143+VIHASINI20@users.noreply.github.com>
2018-01-31 15:50:45 +0530

    c program
    odd number

---
+++ odd number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int num,m=20,n=40;
+clrscr();
+printf("printf odd number in a given range mto n:\n");
+for(num=m,num<=n;num++)
+{
+if(num%2==)
+printf("%d",num);
+}
+getch();
+}

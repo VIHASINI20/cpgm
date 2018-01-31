5feff9fad2d314b020ba4419288ee97cf037a8a1
VIHASINI20 <35221143+VIHASINI20@users.noreply.github.com>
2018-01-31 16:56:30 +0530

    c program
    multiples of 5

---
+++ multiples.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+long int sum=o,i,n;
+clrscr;
+printf("\n pleasegiven the value of n:");
+scanf("%d",&n);
+for(i=1,i<=n,i++);
+{
+printf("\n 5*%ld=%ld"i,5*i);
+}
+getch();
+}

52d4ee09d71322010bdfd2399b15217d427e3fed
VIHASINI20 <35221143+VIHASINI20@users.noreply.github.com>
2018-02-02 16:38:49 +0530

    c program
    minutes nd hours

---
+++ minutes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int num minutes=103;
+int main(void)
+{
+if((num minute/60)==1
+printf("%01d hours",num minutes/60);
+else
+printf("%01d  hours", num minutes/60);
+if((num minutes%60)==1)
+printf("% 01d minutes\n",num mintes%60);
+else
+printf("% 01d minutes\n",num minutes%60);
+return 0;
+}

c09e0f193febdfb5d2570eaa5010914964691323
VIHASINI20 <35221143+VIHASINI20@users.noreply.github.com>
2018-02-12 16:40:31 +0530

    c program

---
+++ string.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10];
+int flag=0,i;
+scanf("%s",a);
+int n=strlen(a);
+for(i=0:i<n;i++)
+{
+if(a[i]>='0'&&a[i]<='a')
+{
+flag=0;
+}
+else
+{
+flag=1;
+printf("no");
+break;
+}
+}
+if(printf("yes");
+}
+return 0;
+}

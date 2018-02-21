3ab3201713650aadb5a679f87600af1819a184d0
VIHASINI20 <35221143+VIHASINI20@users.noreply.github.com>
2018-02-21 16:10:08 +0530

    c prgm

---
+++ num of length.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[1000];
+int n,i,count=1;
+scanf("%s",a);
+n=strlen(a);
+for(i=o;i<n;i++)
+{
+if(a[i]==','&&a[i+1]!=\0]
+{
+count++;
+}
+}
+printf(%d",count);
+return 0;
+}

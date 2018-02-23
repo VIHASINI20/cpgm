679b840d8db9ad1ea7ed71874d995f81deaffc08
VIHASINI20 <35221143+VIHASINI20@users.noreply.github.com>
2018-02-23 15:42:33 +0530

    c prgm

---
+++ number of numeric.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[1000];
+int n,i,count=0;
+sacnf("%s",a);
+n=strlen(0);
+for(i=0;i<n;i++)
+{
+if(a[i]='o',&&a[i]<='a')
+{
+count++;
+}
+}
+printf("%d",count);
+return 0;

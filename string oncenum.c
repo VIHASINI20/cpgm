36f39891f41f9946c13d13f76daf383a84ff3d5f
VIHASINI20 <35221143+VIHASINI20@users.noreply.github.com>
2018-02-28 02:10:29 -0800

    c prgm

---
+++ string oncenum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+char a[100],b[100];
+int i,n ,n1;
+scanf("%s",a);
+scanf("%s",b);
+n=stnlen(a);
+n1=strlen(b);
+if(n=n1)
+{
+printf("%s",b);
+}
+else if(n>n1)
+{
+else
+{
+printf("%s,b);
+}
+return 0;
+}
